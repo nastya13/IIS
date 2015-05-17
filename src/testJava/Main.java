dc4d47fea5ecd3858ec4c38da5cc0ddaee515c62
nastya13 <nastya94-94.08@mail.ru>
2015-05-18 00:10:36 +0300

    tratatt

--- src/testJava/Main.java
+++ src/testJava/Main.java
@@ -2,7 +2,7 @@
  * 
  */
 package testJava;
-import info.debatty.java.stringsimilarity.JaroWinkler;
+
 import info.debatty.java.stringsimilarity.Levenshtein;
 
 import java.io.File;
@@ -19,10 +19,29 @@ import java.util.Scanner;
  */
 public class Main {
 
+	private static String getKey(String[] words, String[] dict, double accuracy) {
+		Levenshtein l = new Levenshtein();
+		if (accuracy > 0)
+			for (int i = 0; i < dict.length; i++) {
+				for (int j = 1; j < words.length; j++) {
+					String str3="",str2="",str="";
+					if(j+2 <words.length) str3 = words[j] + " " + words[j + 1] + " " + words[j + 2];
+					if (j+1<words.length) str2 = words[j] + " " + words[j + 1];
+					str = words[j];
+					if (str3!=null && l.similarity(str3, dict[i]) > 0.7)
+						return dict[i];
+					if (str2!=null && l.similarity(str2, dict[i]) > 0.7)
+						return dict[i];	  
+					if (str!=null && l.similarity(str, dict[i]) > 0.7)
+					
+						return dict[i];
+				}
+			}
+		return null;
+	}
 	private static String getKey(String word, String[] dict, double accuracy)
 	{
 		Levenshtein l = new Levenshtein();
-		//System.out.println(l.similarity("Мобильные Разработки", "мобильных разработок"));
 		if (accuracy>0)
 		for (int i=0;i<dict.length;i++)
 		{
@@ -32,100 +51,162 @@ public class Main {
 		}
 		return null;
 	}
+	private static String checkValue(String word, Map<String, String[]> m) {
+		String[] val;
+		Levenshtein l = new Levenshtein();
+		for (String key : m.keySet()) {
+			val = m.get(key);
+			for (int i = 0; i < val.length-1; i++)
+				if (l.similarity(word, val[i])>0.7)
+					return key;
+		}
+		return null;
+	}
+
+	private static Map<String,String[]> createMap(String[] str)
+	{
+		Map<String,String[]> map = new HashMap<String, String[]>();
+		for (int i = 0; i < str.length; i += 2) {
+			String[] arr = str[i + 1].split(",");
+			for (int j = 0; j < arr.length; j++){
+				arr[j] = (arr[j].substring(0, arr[j].length())).trim();
+			}
+
+			map.put(str[i], arr);
+		}
+		return map;
+	}
 	
-	private static String checkAlias(String word, Map<String,String[]> m)
+	private static String getKeySim(Map<String, String[]> map, String[] ask)
 	{
 		String[] val;
-		for (String key : m.keySet()) 
+		for (String key : map.keySet()) 
 		{
-			//val = new String[m.get(key).length];
-			val = m.get(key);
-			for (int i=1;i<val.length;i++)
-				if (word.contains(val[i]))
-						return key;
+			val = map.get(key);
+			String k = getKey(ask, val, 0.7);
+			if (k!=null)
+			{
+				return key;
+			}
 		}
 		return null;
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException 
+	private static String getKeySim(Map<String, String[]> map, String[] ask, String kkk)
 	{
-		//get ask on words
-		String ask = "Кто знает Саню из лабы Мобильных Разработок";
+		String[] val;
+		for (String key : map.keySet()) 
+		{
+			if (kkk.equals(key))
+				{
+					val = map.get(key);
+					String k = getKey(ask, val, 0.7);
+					if (k!=null)
+					{
+						return k;
+					}
+				}
+		}
+		return null;
+	}
+	
+	private static String findInMap(Map<String, String[]> map, String ask)
+	{
+		String[] val;
+		Levenshtein l = new Levenshtein();
+		for (String key : map.keySet()) 
+		{
+			val = map.get(key);
+			for (int i=0;i<val.length;i++)
+				if (l.similarity(ask, val[i])>0.7) return key;
+		}
+		return null;
+	}
+	public static void main(String[] args) throws FileNotFoundException {
+		// get ask on words
+		String ask = "Кто знает Java и смотрел BlaBla";
 		String[] askWords = ask.split(" ");
 		String questWord = askWords[0];
 		List<String> condition = new ArrayList<String>();
+		Levenshtein l = new Levenshtein();
 		String lab = null;
-		String inter;
-				
+		String lang = null;
+		String film = null;
+		String sport = null;
+		String os = null;
 		
-		//get content of file with name of Man
+
+		// get content of file with name of Man
 		String contentMans = new Scanner(new File("src/testJava/mans.txt")).useDelimiter("\\Z").next();
 		String[] dictionaryMans = contentMans.split("\n");
-		
-		//get content of file with name of Man
-		String contentLabs = new Scanner(new File("src/testJava/labs.txt")).useDelimiter("\\Z").next();
-		String[] dictionaryLabs = contentLabs.split("\n");
-		Levenshtein l = new Levenshtein();
-		for (int i=0;i<dictionaryLabs.length;i++)
-		{
-			for (int j=1;j<askWords.length-1;j++)
-			{
-				String str = askWords[j]+" " +askWords[j+1];//сделать для одного слова тоже
-				String str2 = askWords[j];
-				if (l.similarity(str, dictionaryLabs[i])>0.75 || l.similarity(str2, dictionaryLabs[i])>0.75)
-					lab = dictionaryLabs[i];
-			}
-		}
-		//get content of file with name of Man .useDelimiter("\\Z").next()
-		String contentAlias = (new Scanner(new File("src/testJava/alias.txt")).useDelimiter("\\Z").next()).replaceAll("(\r\n)", " ");
-		
-		
-		String[] dictionaryAlias = contentAlias.split(";");
-		Map<String,String[]> map = new HashMap<String, String[]>();
-		for (int i=0;i<dictionaryAlias.length;i+=2)
-		{
-			String[] arr = dictionaryAlias[i+1].split(" ");
-			for (int j=1;j<arr.length;j++)
-				arr[j] = arr[j].substring(0, arr[j].length()-1);
-				
-			map.put(dictionaryAlias[i], arr);
-		}
 
-//	    for (Map.Entry<String, String[]> entry : map.entrySet()) {
-//	        String key = entry.getKey().toString();
-//	        String[] value = entry.getValue();
-//	        for (int i=0;i<value.length;i++)
-//	        	System.out.print("key= " + key + ", value= " + value[i] + "; ");
-//	    }
-				
-		System.out.print(questWord + " ");
-		for (int i=1; i<askWords.length; i++)
-		{
-			String alias = checkAlias(askWords[i], map);
-			if (alias != null && lab !=null && (l.similarity(askWords[i+1]+askWords[i+2], lab)>0.7||l.similarity(askWords[i+1], lab)>0.7))
+		// get content of file with name of Labs
+		String contentLabs = (new Scanner(new File("src/testJava/labs.txt")).useDelimiter("\\Z").next()).replaceAll("(\r\n)", " ");
+		String[] dictionaryLabs = contentLabs.split(";");
+		Map<String, String[]> mapLabs = createMap(dictionaryLabs);
+		lab = getKeySim(mapLabs,askWords);
+		
+		// get content of file with langs
+		String contentLangs = (new Scanner(new File("src/testJava/lang.txt")).useDelimiter("\\Z").next()).replaceAll("(\r\n)", " ");
+		String[] dictionaryLangs = contentLangs.split(";");
+		Map<String, String[]> mapLang = createMap(dictionaryLangs);
+		lang = getKeySim(mapLang, askWords);
+		
+		// get content of file with aliases 
+		String contentAlias = (new Scanner(new File("src/testJava/alias.txt")).useDelimiter("\\Z").next()).replaceAll("(\r\n)", " ");
+		String[] dictionaryAlias = contentAlias.split(";");
+		Map<String, String[]> mapAlias = createMap(dictionaryAlias);
+
+		
+		//get content of file with name of interests
+		String contentInter = (new Scanner(new File("src/testJava/inter.txt")).useDelimiter("\\Z").next()).replaceAll("(\r\n)", " ");
+		String[] dictionaryInter = contentInter.split(";");
+		Map<String, String[]> mapInter = createMap(dictionaryInter);
+		film = getKeySim(mapInter, askWords, "Film");
+		
+		for (int i = 1; i < askWords.length; i++) {
+			String alias = checkValue(askWords[i], mapAlias);
+			if (alias != null) 
 			{
-				if (alias.equals("ItisLabs"))
+				if (alias.equals("ItisLabs")&& lab != null) 
 				{
-					condition.add(alias + " " +  lab);
-					i+=lab.length();
+					condition.add(alias + " " + lab);
+					i += lab.split(" ").length;
 					lab = null;
 				}
-			}
-			else {
-				if (getKey(askWords[i], dictionaryMans, 0.7)!=null)
+				else if (alias.equals("lang") && lang!=null)
+					{
+						condition.add("programmingLang " + lang);
+					}
+					else if (alias.equals("knows"))
+					{
+						if (getKey(askWords[i+1],dictionaryMans,0.7)!=null)
+						{
+							condition.add(alias +" " + askWords[i+1]);
+							i++;
+						}else if (getKey(askWords[i+2],dictionaryMans,0.7)!=null)
+						{
+							condition.add(alias +" " + askWords[i+2]);
+							i+=2;
+						}
+						i++;
+					}
+			} else if (alias != null && !alias.equals("ItisLabs")) {
+				condition.add(alias);
+			} else {
+				if (getKey(askWords[i], dictionaryMans, 0.7) != null)
 					condition.add("Person " + getKey(askWords[i], dictionaryMans, 0.7));
-				if (getKey(askWords[i], dictionaryLabs, 0.75)!=null)
-					condition.add("ItisLabs "+getKey(askWords[i], dictionaryLabs, 0.75));
+				if (getKey(askWords[i], dictionaryLabs, 0.75) != null)
+					condition.add("ItisLabs " + findInMap(mapLabs,askWords[i]));
+				if (getKey(askWords[i], dictionaryLangs, 0.75) != null)
+					condition.add("ProgrammingLang " + findInMap(mapLang,askWords[i]));
 			}
 		}
+		System.out.print(questWord + " ");
 		System.out.println(condition.toString());
-		//Levenshtein l = new Levenshtein();
-		double k = l.similarity("Саня", "Саню");
-		System.out.println(k);
-		System.out.println("лабы".contains("лаб"));
-        
+		System.out.println(film);
+
 	}
 
-	
-	//System.out.println(l.distanceAbsolute("My string", "My $tring"));
+	// System.out.println(l.distanceAbsolute("My string", "My $tring"));
 }
